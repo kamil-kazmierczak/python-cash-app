dcbc4c52a01750a8dd42a668eaa58fe90d25cf28
Kamilo <kamil.kazmierczak@pm.me>
2024-12-15 23:10:35 +0100

    Modified size of column

--- models/entities.py
+++ models/entities.py
@@ -12,7 +12,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(255))
 
     portfolios = db.relationship('UserPortfolio', back_populates='user')
 
